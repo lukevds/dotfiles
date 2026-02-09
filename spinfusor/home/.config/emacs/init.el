cad58728231d5461825a6ea8d92ab41ff8029295
Lucas Vieira da Silva <lucasvieiradasilva07@gmail.com>
2026-02-09 19:20:08 -0300

    changed spinfusor init.el

--- spinfusor/home/.config/emacs/init.el
+++ spinfusor/home/.config/emacs/init.el
@@ -17,6 +17,8 @@
         ("b" "Atividade do dia" entry (file+olp+datetree "/home/lvds/Documents/diarias.org")
          "* TODO %?")))
 
+(plist-put org-format-latex-options :scale 2.2)
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
@@ -115,90 +117,22 @@ MSearch on English Wikipedia: ")
 
 (require 'geiser)
 (require 'geiser-guile)
+(setq geiser-guile-binary '("guix" "repl"))
+
+(setq erc-nick "muaddibb")
+
+(require 'elfeed)
+
+(setq elfeed-feeds
+      '("http://nullprogram.com/feed/"
+        "http://www.50ply.com/atom.xml"
+        "https://sachachua.com/blog/feed/"
+        "https://planet.emacslife.com/atom.xml"
+        "https://planet.guix.gnu.org/atom.xml"
+        "https://karthinks.com/index.xml"
+        "https://susam.net/feed.xml"))
 
 (load-file (concat user-emacs-directory "./secrets.el"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(safe-local-variable-values
-   '((geiser-repl-per-project-p . t)
-     (eval with-eval-after-load 'yasnippet
-	   (let
-	       ((guix-yasnippets
-		 (expand-file-name "etc/snippets/yas"
-				   (locate-dominating-file
-				    default-directory ".dir-locals.el"))))
-	     (unless (member guix-yasnippets yas-snippet-dirs)
-	       (add-to-list 'yas-snippet-dirs guix-yasnippets)
-	       (yas-reload-all))))
-     (eval with-eval-after-load 'tempel
-	   (if (stringp tempel-path)
-	       (setq tempel-path (list tempel-path)))
-	   (let
-	       ((guix-tempel-snippets
-		 (concat
-		  (expand-file-name "etc/snippets/tempel"
-				    (locate-dominating-file
-				     default-directory
-				     ".dir-locals.el"))
-		  "/*.eld")))
-	     (unless (member guix-tempel-snippets tempel-path)
-	       (add-to-list 'tempel-path guix-tempel-snippets))))
-     (eval with-eval-after-load 'git-commit
-	   (add-to-list 'git-commit-trailers "Change-Id"))
-     (eval setq-local guix-directory
-	   (locate-dominating-file default-directory ".dir-locals.el"))
-     (eval setq-local org-structure-template-alist
-	   (cons '("hs" . "src haskell :results output")
-		 org-structure-template-alist))
-     (org-structure-template-alist cons
-				   '("hs"
-				     . "src haskell :results output")
-				   org-structure-template-alist)
-     (eval progn (require 'lisp-mode)
-	   (defun emacs27-lisp-fill-paragraph (&optional justify)
-	     (interactive "P")
-	     (or (fill-comment-paragraph justify)
-		 (let
-		     ((paragraph-start
-		       (concat paragraph-start
-			       "\\|\\s-*\\([(;\"]\\|\\s-:\\|`(\\|#'(\\)"))
-		      (paragraph-separate
-		       (concat paragraph-separate
-			       "\\|\\s-*\".*[,\\.]$"))
-		      (fill-column
-		       (if
-			   (and
-			    (integerp emacs-lisp-docstring-fill-column)
-			    (derived-mode-p 'emacs-lisp-mode))
-			   emacs-lisp-docstring-fill-column
-			 fill-column)))
-		   (fill-paragraph justify))
-		 t))
-	   (setq-local fill-paragraph-function
-		       #'emacs27-lisp-fill-paragraph))
-     (eval add-to-list 'completion-ignored-extensions ".go")
-     (org-todo-keyword-faces
-      ("CANCELED" :inherit font-lock-comment-face :weight bold))
-     (org-log-done . time)
-     (org-refile-targets
-      (nil :regexp "\\(Concluídas\\|Descartadas\\)"))
-     (eval modify-syntax-entry 43 "'")
-     (eval modify-syntax-entry 36 "'")
-     (eval modify-syntax-entry 126 "'")
-     (geiser-guile-binary "guix" "repl") (geiser-insert-actual-lambda)
-     (org-blank-before-new-entry (heading . auto)
-				 (plain-list-item . auto))
-     (org-list-description-max-indent . 5)
-     (org-list-two-spaces-after-bullet-regexp)
-     (org-confirm-babel-evaluate))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
